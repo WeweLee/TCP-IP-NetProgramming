fdc88ffbb8b42b7eaebb0d9a0fa5b8163695625f
WeweLee <wjb1204@qq.com>
2018-06-30 18:42:49 +0800

    第16章

--- 22-Dup/main.c
+++ 22-Dup/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    int cfd1, cfd2;
+    char str1[] = "Hi~ \n";
+    char str2[] = "It is nice day~ \n";
+
+    // 复制标准输出文件描述符1
+    cfd1 = dup(1);
+    cfd2 = dup2(cfd1, 7);
+
+    printf("fd1 = %d, fd2 = %d \n", cfd1, cfd2);
+    write(cfd1, str1, sizeof(str1));
+    write(cfd2, str2, sizeof(str2));
+
+    close(cfd1);
+    close(cfd2);
+    write(1, str1, sizeof(str1));
+    close(1);
+    write(1, str2, sizeof(str2)); // 无法完成输出
+
     return 0;
 }
--- 22-SepServer/main.c
+++ 22-SepServer/main.c
@@ -1,7 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
-int main()
+#define BUF_SIZE 1024
+
+void error_handling(char *message)
 {
-    printf("Hello World!\n");
+    // fputs向指定的文件写入一个字符串
+    // stderr是Unix标准输出文件
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    // 指针数组 char *argv[]
+    // 数组指针 char (*p)[5] 一个指针指向一个数组
+
+    int serv_sock;
+    int clnt_sock;
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    char message[BUF_SIZE];
+    int str_len, i;
+
+    FILE *readfp;
+    FILE *writefp;
+
+    if(argc != 2)
+    {
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(serv_sock == -1)
+        error_handling("socket() error.");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port=htons(atoi(argv[1])); // 设置监听9190端口
+
+    if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("bind() error.");
+
+    if(listen(serv_sock, 5) == -1)
+        error_handling("listen() error.");
+
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    if(clnt_sock == -1)
+        error_handling("accept() error.");
+    else
+        printf("Client %d Connected.\n ", i);
+
+    readfp = fdopen(clnt_sock, "r");
+    writefp = fdopen(dup(clnt_sock), "w");
+    // 用于向文件输出数据
+    while(!feof(readfp))
+    {
+        fgets(message, BUF_SIZE, readfp);
+        fputs(message, writefp);
+        fflush(writefp);
+    }
+
+    shutdown(fileno(writefp), SHUT_WR);
+    fclose(writefp);
+
+    fputs("writefp close.", stdout);
+    fgets(message, BUF_SIZE, readfp);
+    fputs(message, stdout);
+    fclose(readfp);
+
     return 0;
 }
