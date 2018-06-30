5f8f90044746773f42026b4a423ac0912b8dc017
WeweLee <wjb1204@qq.com>
2018-06-30 18:47:38 +0800

    第15章

--- 21-DesTo/main.c
+++ 21-DesTo/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <fcntl.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    FILE *fp;
+    int fd = open("data.dat", O_WRONLY|O_CREAT|O_TRUNC);
+    if(fd == -1)
+    {
+        fputs("file open error.", stdout);
+        return -1;
+    }
+
+    fp = fdopen(fd, "w");
+    fputs("Network C programming \n", fp);
+    fclose(fp);
     return 0;
 }
